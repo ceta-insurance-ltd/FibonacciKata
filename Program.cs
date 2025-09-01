9db8584a6996ab26c38778b5249409c15be75bd5
Tom Downing <Tom.Downing@ceta.co.uk>
2025-09-01 23:14:35 +0100

    challenge project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+namespace FibonacciKata;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int limit = args.Length > 0 && int.TryParse(args[0], out var input) ? input : 10000;
+
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.Collect();
+
+        var startingMemory = GC.GetTotalMemory(true);
+        var watch = Stopwatch.StartNew();
+
+        var result = Fibonacci(limit);
+
+        watch.Stop();
+        var finalMemory = GC.GetTotalMemory(true);
+        var memoryUsed = finalMemory - startingMemory;
+
+        Console.WriteLine($"\n-- Result: {result}");
+        Console.WriteLine($"-- Time elapsed: {watch.ElapsedMilliseconds} ms");
+        Console.WriteLine($"-- Memory used: {memoryUsed:N0} bytes");
+    }
+
+    static long Fibonacci(int limit)
+    {
+        throw new NotImplementedException("Implement me");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# FibonacciKata
+# Fibonacci Kata
+
+## 🎯 The Challenge
+
+Implement a function that computes the **Nth Fibonacci number**, using any approach:
+
+- Recursion
+- Iteration
+- Memoisation
+- Tail recursion
+- Matrix exponentiation
+
+The project will automatically measure the execution time of the code and measure memory usage.
+
+## 🚀 How to Run
+
+### Requirements
+
+- [.NET 9 SDK](https://dotnet.microsoft.com/en-us/download)
+
+### Clone and Run
+
+```shell
+git clone https://github.com/ceta-insurance-ltd/FibonacciKata.git
+cd FibonacciKata
+dotnet run -- 10
+```
